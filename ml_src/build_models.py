de15424f9f92b4a0c4b7897a7b6c367e2785613f
kv19971 <kshitijverma999@hotmail.com>
2017-12-03 05:05:47 +0800

    added data

--- ml_src/build_models.py
+++ ml_src/build_models.py
@@ -12,9 +12,9 @@ labels_file = "data/labels.csv"
 label_values_file = "data/label_values.json"
 
 # Train and Validation Images
-TRAIN_IMAGES_FOLDER = "data/ClothingAttributeDataset/train/"
-VALID_IMAGES_FOLDER = "data/ClothingAttributeDataset/valid/"
-TEST_IMAGES_FOLDER = "data/ClothingAttributeDataset/test/"
+TRAIN_IMAGES_FOLDER = "data/train/"
+VALID_IMAGES_FOLDER = "data/valid/"
+TEST_IMAGES_FOLDER = "data/test/"
 
 
 if __name__ == "__main__":
@@ -22,15 +22,15 @@ if __name__ == "__main__":
     target_dims = get_attribute_dims(label_values_file)
     use_gpu = is_gpu_available()
     pretrained_conv_model, _, _ = get_pretrained_model("vgg16", pop_last_pool_layer=True, use_gpu=use_gpu)
-
+    train_valid_test_split("data/ClothingAttributeDataset/images/", "data/")
 
     attribute_models = create_attributes_model(AttributeFCN, 512, pretrained_conv_model,
                                     target_dims, 
     #                                 dict(list(target_dims.items())[:3]),
                                     "weights/vgg16-fcn-266-2/",
                                     labels_file, 
-                                     TRAIN_IMAGES_FOLDER, 
-                                     VALID_IMAGES_FOLDER, 
+                                     TRAIN_IMAGES_FOLDER,
+                                     VALID_IMAGES_FOLDER,
                                      num_epochs=50,
                                      is_train=True,
                                      use_gpu=use_gpu)
